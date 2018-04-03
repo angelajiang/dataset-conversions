e0b78e04fbaef1bb4b16ce60ce6dedd6aee2b28b
Angela Jiang <to.ajiang@gmail.com>
2018-04-03 12:23:18 -0400

    Fix details in run

--- tskim-labeler/run.sh
+++ tskim-labeler/run.sh
@@ -4,18 +4,17 @@
 VIDEO_PATH="/Users/angela/src/data/videos/iii/iii-01/vimba_iii_1_2018-3-21_7_scaled.mp4"
 EXPLODED_FRAME_PATH="/tmp/frames/exploded"
 FRAME_PREFIX="iii-01-7-"
-FRAME_NAME_SIZE=8
+FRAME_NAME_SIZE=12
 
-#rm -r $EXPLODED_FRAME_PATH
 #mkdir $EXPLODED_FRAME_PATH
-#ffmpeg -i $VIDEO_PATH $EXPLODED_FRAME_PATH"/"$FRAME_PREFIX"%8d.jpg"
+#ffmpeg -i $VIDEO_PATH $EXPLODED_FRAME_PATH"/"$FRAME_PREFIX"%12d.jpg"
 
 # Create Mainstream-format dataset
 LABELS_DIR="/Users/angela/src/private/labeler/iii_01_buses/iii_01_7"
 LABELS_OUT="/Users/angela/src/private/dataset-conversions/tskim-labeler/labels/iii_01_7.out"
 EVENT_LABEL="bus"
 NO_EVENT_LABEL="no-bus"
-DATASET_DIR="/Users/angela/src/data/image-data/iii-buses"
+DATASET_DIR="/Users/angela/src/data/image-data/iii-buses/iii_01_07"
 
 python make_dataset.py -ld $LABELS_DIR \
                        -lo $LABELS_OUT \
